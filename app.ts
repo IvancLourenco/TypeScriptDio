f51e18026ca13bb4e5245d5cd127733fc6c7ad86
ivancLourenco <ivancampos039@gmail.com>
2024-03-19 19:55:19 -0300

    Aula sobre objetos e interfaces no TypeScript

--- app.ts
+++ app.ts
@@ -2,3 +2,40 @@ let a: String = "A";
 let b: Number = 2;
 let x: Boolean = true;
 let m: any = "qualquer tipo";
+
+
+interface Pessoa {
+    nome: string,
+    idade: number
+    profissao?: string  //opcional não e obrigatorio
+}
+
+const pessoa: Pessoa ={
+    nome: "Ivan",
+    idade:25
+}
+
+const outraPessoa: Pessoa ={
+    nome: "Kamila",
+    idade: 34,
+    profissao: "Nutricionista"
+}
+
+const arrayPessoa: Pessoa[] = [
+    pessoa,
+    outraPessoa
+]
+
+
+const arrayPessoaOutraForma: Array<Pessoa> = [
+    pessoa,
+    outraPessoa
+]
+
+const arrayNumerico: number[] = [
+    1,2,3,4
+]
+
+const arrayString: Array<string> = [
+    "Um", "Array", "de", "Strings"
+]
